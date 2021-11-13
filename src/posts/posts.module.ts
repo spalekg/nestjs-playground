8384853050522bff693b7d255d4784862718704e
Uros Spasojevic <uspasojevic96@gmail.com>
2021-11-13 18:55:49 +0100

    finished part 1

---
+++ src/posts/posts.module.ts
@@ -0,0 +1,10 @@
+import {Module} from "@nestjs/common";
+import {PostsController} from "./controllers/posts.controller";
+import {PostsService} from "./services/posts.service";
+
+@Module({
+    imports: [],
+    controllers: [PostsController],
+    providers: [PostsService]
+})
+export class PostsModule {}
